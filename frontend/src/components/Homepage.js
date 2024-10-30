184783d884bbaffe8aec9d31a890a23f37776e19
Kesh Singh <keshwasya15@gmail.com>
2024-10-30 13:25:27 -0400

    place top artist above top albums

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -64,6 +64,26 @@ function Homepage() {
 
   return (
     <div className="container my-5">
+
+      {/* Top Artists Section */}
+      <h2 className="mt-5">Top 10 Pop Artists</h2>
+      <div className="scroll-container">
+        {artists.map(artist => (
+          <button
+            key={artist.id}
+            href="#"
+            onClick={openSpotifyLink(`spotify:artist:${artist.id}`, `https://open.spotify.com/artist/${artist.id}`)}
+            className="card artist-card"
+            style={{ textDecoration: 'none', color: 'inherit' }}
+          >
+            <img src={artist.images[0]?.url} className="card-img-top" alt={artist.name} />
+            <div className="card-body">
+              <h5 className="card-title">{artist.name}</h5>
+            </div>
+          </button>
+        ))}
+      </div>
+
       {/* Top Albums Section */}
       <h2 className="mt-5">Top 10 Pop Albums</h2>
       <div className="scroll-container">
@@ -79,25 +99,6 @@ function Homepage() {
             <div className="card-body">
               <h5 className="card-title">{album.name}</h5>
               <p className="card-text">By {album.artists[0].name}</p>
-            </div>
-          </button>
-        ))}
-      </div>
-
-      {/* Top Artists Section */}
-      <h2 className="mt-5">Top Artists</h2>
-      <div className="scroll-container">
-        {artists.map(artist => (
-          <button
-            key={artist.id}
-            href="#"
-            onClick={openSpotifyLink(`spotify:artist:${artist.id}`, `https://open.spotify.com/artist/${artist.id}`)}
-            className="card artist-card"
-            style={{ textDecoration: 'none', color: 'inherit' }}
-          >
-            <img src={artist.images[0]?.url} className="card-img-top" alt={artist.name} />
-            <div className="card-body">
-              <h5 className="card-title">{artist.name}</h5>
             </div>
           </button>
         ))}
